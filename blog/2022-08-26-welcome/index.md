1ced4915bb25736666a77891e2bb959ded0560a8
Ben Teichman <ben.l.teichman@gmail.com>
2022-08-26 22:49:08 -0400

    feat: updated documentation

---
+++ blog/2022-08-26-welcome/index.md
@@ -0,0 +1,10 @@
+---
+slug: welcome
+title: Welcome
+authors: [ben]
+tags: [knot]
+---
+
+Knot is a functional programming language that is designed to be used to build cross-platform native applications from a single codebase.
+
+![Knot Snippet](./code.png)
---
+++ docusaurus.config.js
@@ -0,0 +1,160 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const path = require('path');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Knot',
+  tagline: 'Portable application description language',
+  url: 'https://effervescentia.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: '/img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'effervescentia', // Usually your GitHub org/user name.
+  projectName: 'knot-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+    [
+      'docusaurus-preset-shiki-twoslash',
+      {
+        themes: ["min-light", "material-palenight"],
+        langs: [
+          {
+            id: 'knot',
+            scopeName: 'source.knot',
+            path: path.join(__dirname, 'grammar/knot.tmLanguage.json'),
+          },
+          "tsx"
+        ]
+      }
+    ]
+  ],
+
+/**
+ * <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+ */
+  
+  plugins: [
+    () => ({
+      name: 'inject-custom-font',
+      injectHtmlTags() {
+        return {
+          headTags: [
+            { tagName: "link", attributes: { rel: "preconnect", href: "https://fonts.googleapis.com" } },
+            { tagName: "link", attributes: { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true } },
+            { tagName: "link", attributes: { rel: "preload", as: "font", href: "https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" } }
+          ],
+        };
+      }
+    })
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Knot',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'overview',
+            position: 'left',
+            label: 'Reference',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/effervescentia/knot',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Syntax',
+                to: '/docs/syntax',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/effervescentia',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
